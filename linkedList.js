2c68df906b4311cff771258c5f99a55545560473
Azam Gulov <gulovazam@gmail.com>
2023-06-08 15:27:33 +0500

    populate prepend function
    refactor node factory function

--- linkedList.js
+++ linkedList.js
@@ -1,9 +1,32 @@
 const LinkedList = () => {
+  let list = {};
   const append = (value) => {};
-  const prepend = (value) => {};
+  const prepend = (value) => {
+    if (!Object.keys(list).length) {
+      list = node(value);
+      return;
+    }
+    list = node(value, list);
+  };
+
+  return {
+    append,
+    prepend,
+    get list() {
+      return list;
+    },
+  };
 };
 
-const Node = () => {
-  const value = null;
-  const nextNode = null;
-};
+function node(value = null, next = null) {
+  return {
+    value,
+    next,
+  };
+}
+
+// testing
+const newNode = LinkedList();
+newNode.prepend(0);
+newNode.prepend(1);
+console.log(newNode.list);
