05bb118fa1a4bcfc7900d922730db9cd2c01f434
Azam Gulov <gulovazam@gmail.com>
2023-06-10 12:33:03 +0500

    Added tests

---
+++ index.js
@@ -0,0 +1,15 @@
+import LinkedList from "./linkedList.js";
+
+const newLinkedList = LinkedList();
+newLinkedList.prepend(1);
+newLinkedList.prepend(0);
+newLinkedList.append(5);
+newLinkedList.append(8);
+// console.log(newLinkedList.list);
+// console.log(newLinkedList.size);
+// console.log(newLinkedList.head);
+// console.log(newLinkedList.tail);
+// console.log(newLinkedList.at(0));
+// console.log(newLinkedList.contains());
+// console.log(newLinkedList.find(1));
+newLinkedList.toString();
